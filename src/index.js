dae9369343cca42641c2bf40ea15b04ad03506f6
leanbs <davin.yu1110@gmail.com>
2020-03-12 19:02:00 +0700

    test hook 3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hehe hooks hey');
+  res.send('Hehe hooks hey heya');
 })
 
 app.listen(process.env.SERVER_PORT, () => {
